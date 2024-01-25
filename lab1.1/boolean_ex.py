f12fc52bb30956a4e1dc53035aae452754728b79
itbalyq <156638691+itbalyq@users.noreply.github.com>
2024-01-25 20:38:21 +0600

    Add files via upload

---
+++ lab1.1/boolean_ex.py
@@ -0,0 +1,17 @@
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
